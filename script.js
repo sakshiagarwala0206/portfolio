f43b708e5b3462c9866596391d6d68204d8a9832
sakshiagarwala0206 <sakshiagarwala0206@gmail.com>
2025-02-03 03:19:37 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+
+// JavaScript to open and close the modal
+document.querySelectorAll('.more-link').forEach(link => {
+  link.addEventListener('click', function(event) {
+      event.preventDefault();
+      const modalId = this.getAttribute('data-modal');
+      const modal = document.getElementById(modalId);
+      modal.style.display = 'block';
+  });
+});
+
+// Close the modal when the close button is clicked
+document.querySelectorAll('.close-btn').forEach(button => {
+  button.addEventListener('click', function() {
+      const modal = this.closest('.modal');
+      modal.style.display = 'none';
+  });
+});
+
+// Close the modal if the user clicks outside of the modal
+window.onclick = function(event) {
+  if (event.target.classList.contains('modal')) {
+      event.target.style.display = 'none';
+  }
+};
